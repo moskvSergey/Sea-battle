9f97ceaf41d457d5eb13ab243b99585207d84894
TakoySyakoy <121339062+TakoySyakoy@users.noreply.github.com>
2022-12-24 17:25:51 +0700

    Add files via upload

---
+++ LoginController.java
@@ -0,0 +1,111 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import model.JdbcDao;
+
+public class LoginController {
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    public void login(ActionEvent event) throws SQLException {
+
+        Window owner = submitButton.getScene().getWindow();
+
+        if (loginField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                "Please enter your login");
+            return;
+        }
+        if (passwordField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                "Please enter a password");
+            return;
+        }
+
+        String loginId = loginField.getText();
+        String password = passwordField.getText();
+
+        JdbcDao jdbcDao = new JdbcDao();
+        boolean flag = jdbcDao.validate(loginId, password);
+
+        if (!flag) {
+            infoBox("Please enter correct login and password", null, "Failed");
+        } else {
+            infoBox("Login Successful!", null, "Failed");
+            openMain();
+        }
+    }
+
+    public static void infoBox(String infoMessage, String headerText, String title) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    }
+
+    private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+    
+    @FXML
+    private void openReg() {
+    	Window owner = submitButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../view/ViewReg.fxml"));
+        try {
+        	loader.load();
+        }catch(IOException e) {
+        	
+        }
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Регистрация");
+        stage.showAndWait();
+    }
+    
+    private void openMain() {
+    	Window owner = submitButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../view/View.fxml"));
+        try {
+        	loader.load();
+        }catch(IOException e) {
+        	
+        }
+    	owner.hide();
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("SeaBattle");
+        stage.showAndWait();
+    }
+}
