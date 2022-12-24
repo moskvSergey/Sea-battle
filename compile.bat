d6637d4f78d190babdb0d3b85029ba73be87fee8
moskvSergey <116438604+moskvSergey@users.noreply.github.com>
2022-12-24 17:51:27 +0700

    Delete compile.bat

--- compile.bat
+++
@@ -1,8 +0,0 @@
-javac .\SeaBattle\Server.java
-
-javac .\SeaBattle\model\Model.java
-
-javac --module-path .\Libs\javafx-sdk-19\lib --add-modules=javafx.controls,javafx.fxml --class-path ".\SeaBattle" .\SeaBattle\controller\Controller.java
-javac --module-path .\Libs\javafx-sdk-19\lib --add-modules=javafx.controls,javafx.fxml --class-path ".\SeaBattle" .\SeaBattle\Main.java
-
-pause
